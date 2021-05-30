c83aa436ec5e6aa1ffa5dbafeb09e212e46631bb
Martin Masevski <martin.masevski@gmail.com>
2021-05-30 14:22:51 +0200

    Implemented

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-10-grocery-bud
+
+# Todos:
+- fix bug: editing item can be deleted and editing button is still shown
+- change color of editing button to yellow
+- add quantity?
--- src/App.js
+++ src/App.js
@@ -1,9 +1,113 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    return JSON.parse(list);
+  }
+  return [];
+};
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "Please enter value", "danger");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setIsEditing(false);
+      setEditID(null);
+      setName("");
+      showAlert(true, "Item edited", "success");
+    } else {
+      showAlert(true, "Item added on list", "success");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+    setFocus()
+  };
+
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+
+  const clearList = () => {
+    showAlert(true, "Empty list", "danger");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+    showAlert(true, "Item removed", "danger");
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  const setFocus = () => {
+    document.getElementById("submit-btn").focus()
+  }
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  });
+
+  useEffect(() => {
+    setFocus()
+  },[])
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+
+        <h3>grocery bud</h3>
+
+        <div className="form-control">
+          <input
+            id="submit-btn"
+            type="text"
+            className="grocery"
+            placeholder="eg. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
